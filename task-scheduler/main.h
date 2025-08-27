636117da80d6d3deec1edc6a497618dffccbbd3c
yt <o@dm.com>
2025-08-27 12:18:12 -0400

    updated for printf, seperated out LED functions for more organized code, fixed fault caused by misdeclared prototype and other small syntax errors like spacing in linker and double ;;

--- task-scheduler/main.h
+++ task-scheduler/main.h
@@ -2,7 +2,6 @@
  * main.h
  *
  *  Created on: Aug 4, 2025
- *      Author: oogi
  */
 
 #ifndef MAIN_H_
@@ -10,17 +9,17 @@
 
 
 #define CONF_FAULT_REG		0xE000ED24U
-#define ICSR_REG			0xE000ED04U
+#define ICSR_REG		0xE000ED04U
 
-#define MAX_TASKS			5
+#define MAX_TASKS		5
 //Stack Memory Calculations
 //need to reserve space for each task in the stack as well as the scheduler
 #define SIZE_TASK_STACK		1024U //1kB
 #define SIZE_SCHED_STACK	1024U //1kB
 
-#define SRAM_START 			0x20000000U //start of SRAM in processor
-#define SRAM_SIZE 			( (128) * 1024) //total size of SRAM region
-#define SRAM_END 			( (SRAM_START) + (SRAM_SIZE) )
+#define SRAM_START 		0x20000000U //start of SRAM in processor
+#define SRAM_SIZE 		( 128 * 1024) //total size of SRAM region
+#define SRAM_END 		( (SRAM_START) + (SRAM_SIZE) )
 
 //each region next to each other descending as: stack start / t1->t2->t3->t4->scheduler
 #define T1_STACK_START           SRAM_END
@@ -31,33 +30,21 @@
 #define SCHED_STACK_START        ( (SRAM_END) - (5 * SIZE_TASK_STACK) )
 
 //timer definitions
-#define TICK_HZ 			1000U
-#define HSI_CLOCK			16000000U
+#define TICK_HZ 		1000U
+#define HSI_CLOCK		16000000U
 #define SYSTICK_TIM_CLK		HSI_CLOCK
 #define SYSTICK_SVR_REG		0xE000E014U //reload value register
 #define SYSTICK_CSR_REG		0xE000E010U //control status register
 
 //task definitions
-#define INIT_XPSR			0x01000000U //all but T bit
-#define INIT_LR				0xFFFFFFFDU  //based on EXC_RETURN to threadmode w PSP
+#define INIT_XPSR		0x01000000U //all but T bit
+#define INIT_LR			0xFFFFFFFDU  //based on EXC_RETURN to threadmode w PSP
 #define TASK_RUNNING_STATE	0x00
 #define TASK_BLOCKED_STATE	0xFF
 
 //interrupts
-//can use do-while loop to write multiline macro
-#define INTERRUPT_DISABLE() do{__asm volatile ("MOV R0, #0x1"); __asm volatile("MSR PRIMASK, R0");} while(0)	//use primask register
-#define INTERRUPT_ENABLE() 	do{__asm volatile ("MOV R0, #0x0"); __asm volatile("MSR PRIMASK, R0");} while(0)
-
-//LED
-#define LED_GREEN  			12
-#define LED_ORANGE 			13
-#define LED_RED    			14
-#define LED_BLUE   			15
-
-#define DELAY_COUNT_1MS 	1250U
-#define DELAY_COUNT_1S  	(1000U * DELAY_COUNT_1MS)
-#define DELAY_COUNT_500MS  	(500U  * DELAY_COUNT_1MS)
-#define DELAY_COUNT_250MS 	(250U  * DELAY_COUNT_1MS)
-#define DELAY_COUNT_125MS 	(125U  * DELAY_COUNT_1MS)
+//can use do-while loop to write multiline macro //NOTES removed __ from second asm
+#define INTERRUPT_DISABLE() 	do{__asm volatile ("MOV R0, #0x1"); asm volatile("MSR PRIMASK, R0");} while(0)	//use primask register
+#define INTERRUPT_ENABLE() 	do{__asm volatile ("MOV R0, #0x0"); asm volatile("MSR PRIMASK, R0");} while(0)
 
 #endif /* MAIN_H_ */
--- task-scheduler/stm32_startup.c
+++ task-scheduler/stm32_startup.c
@@ -1,7 +1,7 @@
 #include<stdint.h>
 
 #define SRAM_START 	0x20000000U
-#define SRAM_SIZE 	(128 * 1024)
+#define SRAM_SIZE 	(128U * 1024U)
 #define SRAM_END 	((SRAM_START) + (SRAM_SIZE))
 
 #define STACK_START	SRAM_END
@@ -14,15 +14,15 @@ extern uint32_t _la_data;
 extern uint32_t _etext;
 
 //Handler Prototypes
-void Reset_Handler(void);
 int main(void);
 void __libc_init_array(void);
 
 // Default Handler is alias for handlers so in vectortable array, default handler address is stored, so when NMI exception triggers, default handler is executed
-void NMI_Handler(void) 		__attribute__((weak, alias ("Default_Handler")));
-void HardFault_Handler(void) 	__attribute__((weak, alias ("Default_Handler"))); //making funct weak allows for overwritting
+void Reset_Handler(void);
 
-// copy and pasted rest
+//making funct weak allows for overwritting
+void NMI_Handler(void) 		__attribute__((weak, alias ("Default_Handler")));
+void HardFault_Handler(void) 	__attribute__((weak, alias ("Default_Handler"))); 
 void MemManage_Handler 		(void) __attribute__ ((weak, alias("Default_Handler")));
 void BusFault_Handler 		(void) __attribute__ ((weak, alias("Default_Handler")));
 void UsageFault_Handler 	(void) __attribute__ ((weak, alias("Default_Handler")));
@@ -113,13 +113,106 @@ void HASH_RNG_IRQHandler        (void) __attribute__ ((weak, alias("Default_Hand
 void FPU_IRQHandler             (void) __attribute__ ((weak, alias("Default_Handler")));
 
 //Vector Table - Stores MSP and address of handlers
-uint32_t vectors[] __attribute__((section (".isr_vector") ))  = {
+uint32_t vectors[] __attribute__((section(".isr_vector")))   = {
 	STACK_START,
-	(uint32_t)&Reset_Handler,
-	(uint32_t)&NMI_Handler
-	//TODO MAKE REST OF THIS
+	(uint32_t)Reset_Handler,
+	(uint32_t)NMI_Handler,
+	(uint32_t)HardFault_Handler,
+	(uint32_t)MemManage_Handler,
+	(uint32_t)BusFault_Handler,
+	(uint32_t)UsageFault_Handler,
+	0,
+	0,
+	0,
+	0,
+	(uint32_t)SVC_Handler,
+	(uint32_t)DebugMon_Handler,
+	0,
+	(uint32_t)PendSV_Handler,
+	(uint32_t)SysTick_Handler,
+	(uint32_t)WWDG_IRQHandler,
+	(uint32_t)PVD_IRQHandler,         
+	(uint32_t)TAMP_STAMP_IRQHandler,  
+	(uint32_t)RTC_WKUP_IRQHandler,    
+	0,                      
+	(uint32_t)RCC_IRQHandler,         
+	(uint32_t)EXTI0_IRQHandler,       
+	(uint32_t)EXTI1_IRQHandler,       
+	(uint32_t)EXTI2_IRQHandler,       
+	(uint32_t)EXTI3_IRQHandler,       
+	(uint32_t)EXTI4_IRQHandler,       
+	(uint32_t)DMA1_Stream0_IRQHandler,
+	(uint32_t)DMA1_Stream1_IRQHandler,
+	(uint32_t)DMA1_Stream2_IRQHandler,
+	(uint32_t)DMA1_Stream3_IRQHandler,
+	(uint32_t)DMA1_Stream4_IRQHandler,
+	(uint32_t)DMA1_Stream5_IRQHandler,
+	(uint32_t)DMA1_Stream6_IRQHandler,
+	(uint32_t)ADC_IRQHandler,         
+	(uint32_t)CAN1_TX_IRQHandler,     
+	(uint32_t)CAN1_RX0_IRQHandler,    
+	(uint32_t)CAN1_RX1_IRQHandler,    
+	(uint32_t)CAN1_SCE_IRQHandler,    
+	(uint32_t)EXTI9_5_IRQHandler,     
+	(uint32_t)TIM1_BRK_TIM9_IRQHandler,
+	(uint32_t)TIM1_UP_TIM10_IRQHandler,
+	(uint32_t)TIM1_TRG_COM_TIM11_IRQHandler,
+	(uint32_t)TIM1_CC_IRQHandler,     
+	(uint32_t)TIM2_IRQHandler,        
+	(uint32_t)TIM3_IRQHandler,        
+	(uint32_t)TIM4_IRQHandler,        
+	(uint32_t)I2C1_EV_IRQHandler,     
+	(uint32_t)I2C1_ER_IRQHandler,     
+	(uint32_t)I2C2_EV_IRQHandler,     
+	(uint32_t)I2C2_ER_IRQHandler,     
+	(uint32_t)SPI1_IRQHandler,        
+	(uint32_t)SPI2_IRQHandler,        
+	(uint32_t)USART1_IRQHandler,      
+	(uint32_t)USART2_IRQHandler,      
+	(uint32_t)USART3_IRQHandler,      
+	(uint32_t)EXTI15_10_IRQHandler,   
+	(uint32_t)RTC_Alarm_IRQHandler,   
+	(uint32_t)OTG_FS_WKUP_IRQHandler, 
+	(uint32_t)TIM8_BRK_TIM12_IRQHandler,
+	(uint32_t)TIM8_UP_TIM13_IRQHandler,
+	(uint32_t)TIM8_TRG_COM_TIM14_IRQHandler,
+	(uint32_t)TIM8_CC_IRQHandler,     
+	(uint32_t)DMA1_Stream7_IRQHandler,
+	(uint32_t)FSMC_IRQHandler,        
+	(uint32_t)SDIO_IRQHandler,        
+	(uint32_t)TIM5_IRQHandler,        
+	(uint32_t)SPI3_IRQHandler,        
+	(uint32_t)UART4_IRQHandler,       
+	(uint32_t)UART5_IRQHandler,       
+	(uint32_t)TIM6_DAC_IRQHandler,    
+	(uint32_t)TIM7_IRQHandler,        
+	(uint32_t)DMA2_Stream0_IRQHandler,
+	(uint32_t)DMA2_Stream1_IRQHandler,
+	(uint32_t)DMA2_Stream2_IRQHandler,
+	(uint32_t)DMA2_Stream3_IRQHandler,
+	(uint32_t)DMA2_Stream4_IRQHandler,
+	(uint32_t)ETH_IRQHandler,         
+	(uint32_t)ETH_WKUP_IRQHandler,    
+	(uint32_t)CAN2_TX_IRQHandler,     
+	(uint32_t)CAN2_RX0_IRQHandler,    
+	(uint32_t)CAN2_RX1_IRQHandler,    
+	(uint32_t)CAN2_SCE_IRQHandler,    
+	(uint32_t)OTG_FS_IRQHandler,      
+	(uint32_t)DMA2_Stream5_IRQHandler,
+	(uint32_t)DMA2_Stream6_IRQHandler,
+	(uint32_t)DMA2_Stream7_IRQHandler,
+	(uint32_t)USART6_IRQHandler,      
+	(uint32_t)I2C3_EV_IRQHandler,     
+	(uint32_t)I2C3_ER_IRQHandler,     
+	(uint32_t)OTG_HS_EP1_OUT_IRQHandler,
+	(uint32_t)OTG_HS_EP1_IN_IRQHandler,
+	(uint32_t)OTG_HS_WKUP_IRQHandler, 
+	(uint32_t)OTG_HS_IRQHandler,      
+	(uint32_t)DCMI_IRQHandler,        
+	(uint32_t)CRYP_IRQHandler,        
+	(uint32_t)HASH_RNG_IRQHandler,    
+	(uint32_t)FPU_IRQHandler,         
 };
-
 
 //Handlers
 void Reset_Handler(void){
